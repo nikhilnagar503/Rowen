cd66fed85e4e899592fa1e7c3ef8a22c188c3bbb
Nikhil Nagar <nagarn603@gmail.com>
2026-03-26 07:47:01 +0530

    planing feature is removed for now not have been used so removed

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,4 +1,3 @@
 export { sendMessage } from './aiModules/sendMessage';
 export { sendReflection } from './aiModules/reflection';
-export { createAgentPlan, shouldContinueAgent } from './aiModules/planning';
 export { generateOnboardingMessage } from './aiModules/onboarding';
